622d5aa20a17fee099fa0139387a6f8710fa3330
Vagus-art <agustin.favo@gmail.com>
2020-12-19 16:24:13 -0300

    test db

--- handlers/testdb/index.py
+++ handlers/testdb/index.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import rds_config
+import json
 import pymysql
 #rds settings
 rds_host  = rds_config.rds_host
@@ -12,7 +13,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 try:
-    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=20)
 except pymysql.MySQLError as e:
     logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
     logger.error(e)
@@ -20,23 +21,28 @@ except pymysql.MySQLError as e:
 
 logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 def handler(event, context):
-    """
-    This function fetches content from MySQL RDS instance
-    """
+    # """
+    # This function fetches content from MySQL RDS instance
+    # """
 
-    item_count = 0
+    # item_count = 0
 
-    with conn.cursor() as cur:
-        cur.execute("create table Employee ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))")
-        cur.execute('insert into Employee (EmpID, Name) values(1, "Joe")')
-        cur.execute('insert into Employee (EmpID, Name) values(2, "Bob")')
-        cur.execute('insert into Employee (EmpID, Name) values(3, "Mary")')
-        conn.commit()
-        cur.execute("select * from Employee")
-        for row in cur:
-            item_count += 1
-            logger.info(row)
-            #print(row)
-    conn.commit()
+    # with conn.cursor() as cur:
+    #     cur.execute("create table Employee ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))")
+    #     cur.execute('insert into Employee (EmpID, Name) values(1, "Joe")')
+    #     cur.execute('insert into Employee (EmpID, Name) values(2, "Bob")')
+    #     cur.execute('insert into Employee (EmpID, Name) values(3, "Mary")')
+    #     conn.commit()
+    #     cur.execute("select * from Employee")
+    #     for row in cur:
+    #         item_count += 1
+    #         logger.info(row)
+    #         #print(row)
+    # conn.commit()
 
-    return "Added %d items from RDS MySQL table" %(item_count)
+    data = {
+            'output': 'hola'#"Added %d items from RDS MySQL table" %(item_count)
+        }
+    return json.dumps({'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}})
--- handlers/testdb/rds_config.py
+++ handlers/testdb/rds_config.py
@@ -1,5 +1,7 @@
 #config file containing credentials for RDS MySQL instance
-rds_host = "username"
-db_username = "username"
-db_password = "password"
-db_name = "ExampleDB" 
+import os
+
+rds_host = os.getenv('RDS_HOST')
+db_username = os.getenv('RDS_DB_USERNAME')
+db_password = os.getenv('RDS_DB_PASSWORD')
+db_name = os.getenv('RDS_DB_NAME')
